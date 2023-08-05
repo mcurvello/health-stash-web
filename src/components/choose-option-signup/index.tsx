d1d8fd46b5074c3717b564f456695c14b29babdc
Laércio <jose.laercio.bezerra@gmail.com>
2023-08-05 17:42:12 -0300

    change initial state

--- src/components/choose-option-signup/index.tsx
+++ src/components/choose-option-signup/index.tsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import BaseRadio from '@/components/base-radio';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Container, ContainerInputRadio, Title } from './styles';
 
 const ChooseOptionSignup = () => {
-  const [option, setOption] = useState('doctor'); 
+  const location = useLocation();
+
+  const initialOption = location.pathname === '/signup/patient' ? 'patient' : 'doctor';
+
+  const [option, setOption] = useState(initialOption); 
 
   const isDoctor = option === 'doctor';
   
   const navigate = useNavigate();
-
+  
   function handleChange(value: string) {
     setOption(value);
     const route = value === 'doctor' ? '/signup' : '/signup/patient';
